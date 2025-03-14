7714f16ebc74e5fe04281f870af8f0cb55405a1e
Dongah Kim <donga0223@gmail.com>
2025-03-14 18:32:54 -0400

    get ensemble output code

--- src/get_ensemble_output.R
+++ src/get_ensemble_output.R
@@ -2,61 +2,40 @@ library(hubUtils)
 library(hubVis)
 library(hubEnsembles)
 library(hubData)
-
-
-library(gh)
-library(purrr)
-library(readr)
 library(dplyr)
 
-# GitHub API request to list files in the "model-output" folder
-repo <- "reichlab/flu-metrocast"
-path <- "model-output"
 
-files <- gh("GET /repos/:owner/:repo/contents/:path",
-            owner = "reichlab",
-            repo = "flu-metrocast",
-            path = "model-output")
+hub_path <- "/Users/dk29776/Dropbox/UTAustin/flu-metrocast"
+hub_con <- connect_hub(hub_path)
 
-# Extract only model names (folder names)
-model_names <- map_chr(files, "name")
+model_names <- hub_con %>%  
+  select(model_id) %>% 
+  distinct() %>% 
+  collect()
 
-# remove readme.md 
-model_names <- model_names[!grepl("README.md", model_names)]
+model_names
 
-read_forecast <- function(model, reference_date) {
-  url <- paste0("https://raw.githubusercontent.com/reichlab/flu-metrocast/main/model-output/",
-                model, "/", reference_date, "-", model, ".csv")
-  
-  # Try reading the file, if it fails, return NULL
-  tryCatch({
-    read_csv(url) %>% mutate(model_id = model)
-  }, error = function(e) {
-    message("File not found: ", url)
-    return(NULL)  # Skip file if not found
-  })
-}
+reference_date = "2025-03-15"
 
-
-
-reference_date <- "2025-03-01"  # Example date, modify as needed
-
-# Corrected function call with explicit reference_date
-forecast_output <- map_dfr(model_names, ~read_forecast(.x, reference_date))
-
-
-
-linear_pool_norm <- forecast_output |>
-  hubEnsembles::linear_pool(model_id = "linear-pool-normal") |>
+linear_pool_norm <- hub_con %>%
+  filter(
+    reference_date == reference_date,
+    !(model_id %in% c("epiENGAGE-ensemble_mean", "epiENGAGE-lop_norm"))
+  ) %>%
+  collect_hub() %>%
+  hubEnsembles::linear_pool(model_id = "linear-pool-normal")|>
   select(-model_id)
 write.csv(linear_pool_norm, paste("model-output/epiENGAGE-lop_norm/",reference_date, "-epiENGAGE-lop_norm.csv", sep = ""), row.names = FALSE)
 
-mean_ens <- forecast_output |>
-  hubEnsembles::simple_ensemble(
-    model_id = "simple-ensemble-mean"
-  )|>
+
+mean_ens <- hub_con %>%
+  filter(
+    reference_date == reference_date,
+    !(model_id %in% c("epiENGAGE-ensemble_mean", "epiENGAGE-lop_norm"))
+  ) %>%
+  collect_hub() %>%
+  hubEnsembles::simple_ensemble(model_id = "simple-ensemble-mean")|>
   select(-model_id)
 
 write.csv(mean_ens, paste("model-output/epiENGAGE-ensemble_mean/",reference_date, "-epiENGAGE-ensemble_mean.csv", sep = ""), row.names = FALSE)
-
 

32fc3e2eab2ffb069c51bd8da9c0c4da69b8ef20
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-11-19 13:50:49 -0500

    Use location instead of name, restore target data

--- src/NC_clean.R
+++ src/NC_clean.R
@@ -120,17 +120,6 @@ nc_time_series <- NC_clean() %>%
   mutate(as_of_date = as.Date(as_of_date)) %>%
   mutate(target = "Flu ED visits pct") %>%
   rename(as_of = as_of_date) %>%
-  mutate(location = case_match(
-    location,
-    "nenc" ~ "Northeastern, NC",
-    "senc" ~ "Southeastern, NC",
-    "fay-area" ~ "Fayetteville Area, NC",
-    "rtp-area" ~ "Research Triangle Park Area, NC",
-    "triad-area" ~ "Triad Area, NC",
-    "wnc" ~ "Western, NC",
-    "clt-area" ~ "Charolette Area, NC",
-    .default = location
-  )) %>%
   relocate(as_of, location, target, target_end_date, observation)
 
 nc_latest_data <- nc_time_series %>%

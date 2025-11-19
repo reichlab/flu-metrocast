0b0525c7d85f9a58006ea8d741b8e26d4ded4a8f
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-11-19 11:57:37 -0500

    Adjust NC script to match quoting

--- src/NC_clean.R
+++ src/NC_clean.R
@@ -103,9 +103,14 @@ merge_and_write_csv <- function(data, file) {
   locations <- unique(data$location)
   original_data <- read_csv(file) %>%
     filter(!location %in% locations)
-  new_data <- bind_rows(data, original_data) %>%
-    arrange(across(!any_of(c("oracle_value", "observation"))))
-  write_csv(new_data, file)
+  new_data <- bind_rows(original_data, data)
+  write.table(
+    new_data,
+    file = file,
+    sep = ",",
+    row.names = FALSE,
+    quote = which(names(new_data) %in% c("location", "target"))
+  )
 }
 
 

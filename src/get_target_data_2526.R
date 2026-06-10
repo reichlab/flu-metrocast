a0eefc58257b2ef3e9f17389b86d3041e7f86abc
Dongah Kim <donga0223@gmail.com>
2026-06-10 13:24:41 -0400

    add June for oracle output

--- src/get_target_data_2526.R
+++ src/get_target_data_2526.R
@@ -12,7 +12,7 @@ NY_ili_ED_daily_to_weekly_ts <- function(df_daily, as_of = NULL){
   }else{
     as_of = as_of
   }
-  
+
   # Clean the newly downloaded data
   df <- df_daily %>%
     filter(Dim2Value == "All age groups", #,
@@ -35,7 +35,7 @@ NY_ili_ED_daily_to_weekly_ts <- function(df_daily, as_of = NULL){
       observation = X
     ) %>%
     select(as_of, WeekStart, location, target, target_end_date, observation)
-  
+
   # Aggregate the data to weekly summaries
   df_weekly <- df %>% group_by(as_of, location, WeekStart, target, target_end_date) %>%
     mutate(observation = as.numeric(observation)) %>%
@@ -43,8 +43,8 @@ NY_ili_ED_daily_to_weekly_ts <- function(df_daily, as_of = NULL){
     select(-WeekStart) %>%
     arrange(target_end_date) %>%
     mutate(location = tolower(gsub(" ", "-", location)),
-           observation = observation*100) 
-  
+           observation = observation*100)
+
   return(df_weekly)
 }
 
@@ -75,7 +75,7 @@ NSSP_hsa <- pub_covidcast(
   source = "beta_nssp_github",
   signals = "pct_ed_visits_influenza",
   geo_type = "hsa_nci",
-  time_type = "week",              
+  time_type = "week",
 )
 max(NSSP_hsa$time_value)
 
@@ -84,14 +84,14 @@ NSSP_state <- pub_covidcast(
   source = "beta_nssp_github",
   signals = "pct_ed_visits_influenza",
   geo_type = "state",
-  time_type = "week",              
+  time_type = "week",
 )
 max(NSSP_state$time_value)
 
 
 locations <- read_csv("auxiliary-data/locations.csv")
 hsa_list <- locations %>%
-  filter(location_type == "hsa_nci_id", 
+  filter(location_type == "hsa_nci_id",
          location != 'nyc',
          original_location_code != 'All')
 
@@ -100,10 +100,10 @@ state_list <- locations %>%
          location != 'north-carolina') %>%
   mutate(state_abb2 = tolower(state_abb))
 
-NSSP_hsa_df <- NSSP_hsa %>% 
+NSSP_hsa_df <- NSSP_hsa %>%
   filter(geo_value %in% hsa_list$original_location_code) %>%
   left_join(hsa_list %>%
-              select(location, original_location_code, population), 
+              select(location, original_location_code, population),
             by = c("geo_value" = "original_location_code")) %>%
   mutate(as_of = Sys.Date(),
          target = "Flu ED visits pct",
@@ -111,10 +111,10 @@ NSSP_hsa_df <- NSSP_hsa %>%
   select(as_of, location, target, target_end_date, observation = value)
 
 
-NSSP_state_df <- NSSP_state %>% 
+NSSP_state_df <- NSSP_state %>%
   filter(geo_value %in% state_list$state_abb2) %>%
   left_join(state_list %>%
-              select(location, state_abb2, population), 
+              select(location, state_abb2, population),
             by = c("geo_value" = "state_abb2")) %>%
   mutate(as_of = Sys.Date(),
          target = "Flu ED visits pct",
@@ -122,9 +122,9 @@ NSSP_state_df <- NSSP_state %>%
   select(as_of, location, target, target_end_date, observation = value)
 
 
-nssp1 <- rbind(NSSP_hsa_df, NSSP_state_df) 
+nssp1 <- rbind(NSSP_hsa_df, NSSP_state_df)
 nssp <- nssp1 %>%
-  mutate(observation = round(observation,2)) 
+  mutate(observation = round(observation,2))
 
 ##########################################
 ##########################################
@@ -144,7 +144,7 @@ NSSP_ts <- nssp %>%
   filter(target_end_date >= '2024-08-01') %>%
   distinct()
 
-today_time_series <-  rbind(NY_ts, NSSP_ts) 
+today_time_series <-  rbind(NY_ts, NSSP_ts)
 new_time_series <- rbind(time_series, today_time_series)
 
 write.csv(new_time_series, "target-data/time-series.csv", row.names = FALSE)
@@ -189,7 +189,8 @@ target_end_date_list <- c("2025-11-22", "2025-11-29", "2025-12-06", "2025-12-13"
                           "2026-02-14", "2026-02-21", "2026-02-28", "2026-03-07",
                           "2026-03-14", "2026-03-21", "2026-03-28", "2026-04-04",
                           "2026-04-11", "2026-04-18", "2026-04-25", "2026-05-02",
-                          "2026-05-09", "2026-05-16", "2026-05-23", "2026-05-30")
+                          "2026-05-09", "2026-05-16", "2026-05-23", "2026-05-30",
+                          "2026-06-06", "2026-06-13")
 
 oracle_output <- read_csv("target-data/oracle-output.csv")
 
